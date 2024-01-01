5d2046dc50ee700195775a654127965401bc622b
Yankl-Peretz Blum <yakovblum@gmail.com>
2024-01-01 23:25:14 +0200

    develop Images class
    add function to return number of pages in given issue, caching results to file. Also delete some whitespace.

--- src/Main/Images.php
+++ src/Main/Images.php
@@ -1,23 +1,43 @@
-<?php 
+<?php
 
 namespace Yugntruf\ZhurnalArkhiv\Main;
 
 use DI\Attribute\Inject;
 
 class Images {
-	
+
 	public function __construct(
 	#[Inject('images_folder')] private string $images_folder,
-	#[Inject('image_file_pattern')] private string $images_file_pattern
+	#[Inject('image_file_pattern')] private string $images_file_pattern,
+	private string $cache_filename = ARKHIV_PLUGIN_DIR . 'num2pages.txt',
 	) {}
-	
-	
+
 	/**
-	* 
-	* Return the number of pages a give issue has 
+	*
+	* Return the number of pages a give issue has
 	* based on the number of images.
 	*
-	* @param string $numer 
+	* @param string $numer
+	*
+	* @return array
+	*/
+	public function numer2pages($numer) {
+		$cache = $this->cached_num2pages();
+
+		if (false == $cache)
+			$cache = $this->cached_num2pages($this->numer2pages_thehardway());
+
+		if (false === $cache)
+			throw new Exception("Error writing file {$this->cache_filename}.");
+
+		return $cache[$numer];
+
+	}
+
+	/**
+	*
+	* Return the number of pages a give issue has
+	* based on the number of images.
 	*
 	* @return array
 	*/
@@ -28,26 +48,39 @@ class Images {
 		$num2pages = array();
 		foreach (glob($search_pattern) as $filename) {
 			//the pages are found between the second and third periods in the filename
-			//This is not independent of the file pattern, but that would require 
+			//This is not independent of the file pattern, but that would require
 			//more abstraction, regex which is not worth it since practically
-			//speaking this will never change. 
+			//speaking this will never change.
 			$pieces = explode('.', basename($filename));
-			$numer = $pieces[1];
-			$zaytl = $pieces[2];
-			$num2pages[$numer][] = intval($zaytl);
+			$numer = (string) $pieces[1];
+			$zaytl = (int) $pieces[2];
+			$num2pages[$numer][] = $zaytl;
 		}
-		
-		// Make sure no pages are missing before the end 
+
+		// Make sure no pages are missing before the end
 		// and that the number of pages are even
 		foreach ($num2pages as $numer => $numarray) {
-			if (max ($numarray) == count($numarray) and max($numarray) % 2 == 0 ) 
-				continue ; 
+			if (max ($numarray) == count($numarray) and max($numarray) % 2 == 0 )
+				continue ;
 
 			throw new Exception("It seems there are pages missing in issue $numer.");
 		}
-		
-		$num2count = array(); 
+
+		$num2count = array();
 		foreach ($num2pages as $numer => $pages) $num2count[$numer] = count($pages);
 		return $num2count;
+	}
+
+	private function cached_num2pages(array $num2pages = null) {
+		if (is_null ( $num2pages ) ) {
+			if (!file_exists($this->cache_filename))
+				return false;
+			$num2pages = unserialize(file_get_contents($this->cache_filename));
+		} else {
+			if (false === file_put_contents($this->cache_filename, serialize($num2pages)))
+				return false;
+		}
+
+		return $num2pages;
 	}
 }
