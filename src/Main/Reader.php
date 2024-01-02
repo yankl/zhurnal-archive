103d68d7653d097debeca972ae96aa9cb0804e04
Yankl-Peretz Blum <yakovblum@gmail.com>
2024-01-02 21:48:58 +0200

    new class Reader
    which will be in charge of creating BookReader

---
+++ src/Main/Reader.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Yugntruf\ZhurnalArkhiv\Main;
+
+use DI\Attribute\Inject;
+
+class Reader {
+
+	public function __construct( private Images $images ) {}
+
+	/**
+	* Outputs html for bookreader for given issue.
+	*
+	* @param string $numer
+	*
+	* @return string
+	*/
+	public function html($numer): string {
+		$title = "יוגנטרוף נ׳ $numer";
+
+		ob_start();
+		include ARKHIV_PLUGIN_DIR . '/templates/zhurnal-reader.php';
+		$output = ob_get_contents();
+		ob_end_clean();
+		return $output;
+	}
+
+	public function data_json($numer): string {
+		$num_pages = $this->images->numer2pages($numer);
+
+		$data_array = array();
+		$spread = array();
+
+		for ($page = 1; $page <= $this->images->numer2pages($numer); $page++) {
+			$page_object = new \stdClass();
+			$page_object->uri = $this->images->img_uri($numer, $page);
+			$spread[] = $page_object;
+
+			if ( 1 == $page % 2 ) {
+				$data_array[] = $spread;
+				$spread = [];
+			}
+		}
+
+		if ( $spread )
+			$data_array[] = $spread;
+
+		return json_encode($data_array, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+	}
+}
