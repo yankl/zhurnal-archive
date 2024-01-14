b71b92abad40ca5ff61db51ff48c6684fcbb61aa
Yankl-Peretz Blum <yakovblum@gmail.com>
2024-01-14 15:29:34 +0200

    Delete unneeded files

--- zhurnal/zhurnalwp.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$ZHURNALPATH = ABSPATH . '/../zhurnal/';
-require_once($ZHURNALPATH . 'zhurnal.inc.php');
-$xmlpath = $ZHURNALPATH . 'zhurnaln.xml';
-$xslpath = $ZHURNALPATH . 'zhurnalindex.xsl';
-
-echo_xml_with_xsl($xmlpath, $xslpath);
-?>
